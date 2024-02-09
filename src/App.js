9b410459e46e014fe10c6b051a344e246471c17f
pradeep <pradeep.9997@gmail.com>
2024-02-09 21:45:01 +0530

    latest Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [counter,setCouter]=useState(1); 
+  let [passwordStatus,setPasswordStatus]=useState(false)
+  // let count=1;
+
+  // let chnageCounter=()=>{
+  //     counter=counter+1
+  //     setCouter(counter)
+
+  //   // count=count+1;
+  //   // console.log(count)
+  // }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>  {counter}   </h1>
+        <button onClick={()=>setCouter(counter+1)}>Counter</button>
+
+          <br/>
+
+          <input type={ (passwordStatus) ? 'text' : 'password' } /> 
+          
+          <button onClick={()=>setPasswordStatus(!passwordStatus)}> 
+          { (passwordStatus) ? 'hide' : 'show' }
+           </button>
+           
+
+
+
     </div>
   );
 }
